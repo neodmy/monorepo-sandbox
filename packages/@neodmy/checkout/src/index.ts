de2b0a00118d0e061dde68069d8681012a9cc8e5
David Yusta <david.yusta@guidesmiths.com>
2023-04-18 14:14:25 +0200

    refactor: scope packages

--- packages/@neodmy/checkout/src/index.ts
+++ packages/@neodmy/checkout/src/index.ts
@@ -1,5 +1,5 @@
-import UserApi from 'user';
-import OrderApi from 'order';
+import UserApi from 'packages/@neodmy/user';
+import OrderApi from 'packages/@neodmy/order';
 
 export default class Checkout {
 	constructor(
